0ddc5afd2ffc55f723dcd3e779d90cf5dc06e4ce
raulcodes <rcamacho96@gmail.com>
2018-11-13 18:37:12 -0600

    remove hamburger in favor of tabs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
 import React, { Component } from 'react';
 import styles from './App.css';
-import Hamburger from './components/Hamburger/Hamburger';
 import Greeting from './components/Greeting/Greeting';
 import Lights from './components/Lights/Lights.js';
 import Tv from './components/TV/Tv.js';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom'
+import SwipeableViews from 'react-swipeable-views';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 class App extends Component {
+  state = {
+    slideValue: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ slideValue: value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ slideValue: index });
+  };
+
   render() {
     return (
-      <Router>
-        <div className="App" id="outer_container">
-          <Hamburger pageWrapId={"menu_wrap"} outerContainerId={ "outer_container" } />
-          <div id="menu_wrap" className={styles.content}>
-            <Switch>
-              <Route exact path="/" render={() => <Greeting name="Raul"/>} />
-              <Route path="/lights" component={Lights} />
-              <Route path="/tv" component={Tv} />
-            </Switch>
+        <div className="App">
+          <AppBar position="static" color="default">
+            <Tabs
+              value={this.state.slideValue}
+              onChange={this.handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              fullWidth
+            >
+              <Tab label="Home" />
+              <Tab label="Lights" />
+              <Tab label="TV" />
+            </Tabs>
+          </AppBar>
+          <div className={styles.content}>
+            <SwipeableViews
+              index={this.state.slideValue}
+              onChangeIndex={this.handleChangeIndex}
+            >
+              <Greeting name="Amigo" />
+              <Lights />
+              <Tv />
+            </SwipeableViews>
           </div>
         </div>
-      </Router>
     );
   }
 }
